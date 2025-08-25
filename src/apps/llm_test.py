9ea7234a999ccb2883ef1b7334be9ad03a0e64a0
Viyachikhh <vyachik2k1@gmail.com>
2025-08-25 12:45:42 +0300

    added test script for llm running

---
+++ src/apps/llm_test.py
@@ -0,0 +1,12 @@
+import asyncio
+from src.services.llm import OllamaLLMService, QuestionDTO
+
+
+async def main():
+    llm = OllamaLLMService(model_name="llama3.2:3b", ollama_base_url="http://localhost:11434")
+    result = await llm.execute(QuestionDTO(text="Hi, how are you?", history=[]))
+    print(result)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
